00274ae76f63745296df381d364ee9ee8fe4fd13
usuario <usuario@DESKTOP-OR0BI5F.mshome.net>
2020-11-18 00:24:11 +0100

    Intento subir de nuevo ejercicios

---
+++ PSP_jesusrodenas/src/ejerciciosCasa/ejercicioDos.java
@@ -0,0 +1,18 @@
+package ejerciciosCasa;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ejercicioDos {
+	public static void main(String[] args) throws IOException {
+	File dir = new File("/ejercicios/fichero.txt");
+	String[] ficheros = dir.list();
+	if (ficheros == null) {
+		  System.out.println("No hay ficheros en el directorio especificado");}
+		else { 
+		  for (int x=0;x<ficheros.length;x++)
+		    System.out.println(ficheros[x]);
+		}
+}
+}
+	
