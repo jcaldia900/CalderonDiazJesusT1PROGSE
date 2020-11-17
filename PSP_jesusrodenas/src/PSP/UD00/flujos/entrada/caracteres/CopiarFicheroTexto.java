cb451d7c6d8e067b8a4bc8dfa286fdc5dd69ecae
usuario <usuario@DESKTOP-OR0BI5F.mshome.net>
2020-11-17 23:49:18 +0100

    Ejercicios nuevos 17.11

--- PSP_jesusrodenas/src/PSP/UD00/flujos/entrada/caracteres/CopiarFicheroTexto.java
+++ PSP_jesusrodenas/src/PSP/UD00/flujos/entrada/caracteres/CopiarFicheroTexto.java
@@ -8,12 +8,38 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- * @author JESUS
- *
- */
-public class CopiarFicheroTexto {
+public class CopiarFicheroTexto extends Codificador {
+	public static void main(String[] args) {
 
-	// Código
+		String nombreFichero = "src/fichero_a_cifrar.txt";
 
+		imprimirFicheroPorLineas(nombreFichero);
+		codifica(nombreFichero);
+		decodifica(nombreFichero);
+
+	}
+
+	private static void imprimirFicheroPorLineas(String nombreFichero) {
+		String linea;
+		String nombreFicheroCopia = "src/ficheroCodificado";
+		try (BufferedReader bufferLectura = new BufferedReader(new FileReader(nombreFichero));
+				BufferedWriter filtroEscritura = new BufferedWriter(new FileWriter(nombreFicheroCopia + ".txt"));) {
+
+			linea = bufferLectura.readLine();
+			while (linea != null) {
+				System.out.println(linea);
+				if(linea.isEmpty()) {filtroEscritura.write(decodifica(linea));
+				}else {filtroEscritura.write(codifica(linea));}
+				filtroEscritura.newLine();
+
+				linea = bufferLectura.readLine();
+
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("No existe el fichero " + nombreFichero);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
